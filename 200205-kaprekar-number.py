117ea790bf9c408974b6d71a863a0172f7943203
niko.juliyanto <niko.juliyanto@go-jek.com>
2020-02-05 10:55:49 +0700

    init kaprekar number problem

---
+++ 200205-kaprekar-number.py
@@ -0,0 +1,12 @@
+KAPREKAR_CONSTANT = 6174
+
+def num_kaprekar_iterations(n):
+  # Fill this in.
+  return 0
+
+print(num_kaprekar_iterations(123))
+# 3
+# Explanation:
+#  3210 - 123 = 3087
+#  8730 - 0378 = 8352
+#  8532 - 2358 = 6174 (3 iterations)
