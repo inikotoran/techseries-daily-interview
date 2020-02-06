5a63e79fcfb1fd5628918ef7b557cc8475488170
niko.juliyanto <niko.juliyanto@go-jek.com>
2020-02-06 08:33:49 +0700

    init skyline problem

---
+++ 200206-skyline.py
@@ -0,0 +1,12 @@
+def generate_skyline(buildings):
+  # Fill this in.
+  return [0, 0]
+
+#            2 2 2
+#            2 2 2
+#        1 1 2 2 2 1 1
+#        1 1 2 2 2 1 1
+#        1 1 2 2 2 1 1
+# pos: 1 2 3 4 5 6 7 8 9
+print(generate_skyline([(2, 8, 3), (4, 6, 5)]))
+# [(2, 3), (4, 5), (7, 3), (9, 0)]
