29067f661fa1eec61cc12d52bbf95b1534d6ecee
niko.juliyanto <niko.juliyanto@go-jek.com>
2020-02-07 10:47:27 +0700

    implement skyline solution

--- 200206-skyline.py
+++ 200206-skyline.py
@@ -1,6 +1,31 @@
 def generate_skyline(buildings):
   # Fill this in.
-  return [0, 0]
+  # check overlap with previous building 
+  #   if(prev[right] > now[left]):
+  #     prev[right] = now[left] - 1
+  #   if(prev[right] > now[right]):
+  #     buildings.append((now[right]+1, prev[right], prev[height]))
+  iterator = 0
+  while iterator < len(buildings):
+    now = buildings[iterator]
+    if(iterator != 0):
+      prev = buildings[iterator-1]
+      if(prev[1] > now[0]):
+        newPrev = (prev[0], now[0] - 1, prev[2])
+        buildings[iterator-1] = newPrev
+      if(prev[1] > now[1]):
+        buildings.append((now[1]+1, prev[1], prev[2]))
+    iterator += 1
+  result = []
+  right = 0
+  for building in buildings:
+    start = building[0]
+    right = building[1]
+    height = building[2]
+    result.append((start, height))
+  if (right < 9):
+    result.append((right+1, 0))
+  return result
 
 #            2 2 2
 #            2 2 2
