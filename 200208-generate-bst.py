0696bd65edaa6465bd0b8e9dc3af6c19bf60dedb
niko.juliyanto <niko.juliyanto@go-jek.com>
2020-02-08 11:33:33 +0700

    init generate bst problem

---
+++ 200208-generate-bst.py
@@ -0,0 +1,33 @@
+class Node:
+  def __init__(self, value, left=None, right=None):
+    self.value = value
+    self.left = left
+    self.right = right
+
+  def __str__(self):
+    result = str(self.value)
+    if self.left:
+      result = result + str(self.left)
+    if self.right:
+      result = result + str(self.right)
+    return result
+
+def generate_bst(n):
+  # Fill this in.
+  return [0]
+
+for tree in generate_bst(3):
+  print(tree)
+
+# Pre-order traversals of binary trees from 1 to n.
+# 123
+# 132
+# 213
+# 312
+# 321
+
+#   1      1      2      3      3
+#    \      \    / \    /      /
+#     2      3  1   3  1      2
+#      \    /           \    /
+#       3  2             2  1
